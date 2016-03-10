a84f4274e7dbce164cff6f0e983c4d2fc1bb094c
Arjen Smits <Deathraven163@gmail.com>
2016-03-10 16:39:47 +0100

    Update build.fsx
    CI test and build.fsx update

--- build.fsx
+++ build.fsx
@@ -19,7 +19,7 @@ cd __SOURCE_DIRECTORY__
 
 let product = "Akka.NET"
 let authors = [ "Akka.NET Team" ]
-let copyright = "Copyright © 2013-2016 Akka.NET Team"
+let copyright = "Copyright Â© 2013-2016 Akka.NET Team"
 let company = "Akka.NET Team"
 let description = "Akka.NET is a port of the popular Java/Scala framework Akka to .NET"
 let tags = ["akka";"actors";"actor";"model";"Akka";"concurrency"]
@@ -346,7 +346,7 @@ Target "HelpNuget" <| fun _ ->
 
 // nuget dependencies
 "CleanNuget" ==> "CreateNuget"
-"CleanNuget" ==> "BuildRelease" ==> "Nuget"
+"CreateNuget" ==> "BuildRelease" ==> "Nuget"
 
 Target "All" DoNothing
 "BuildRelease" ==> "All"
