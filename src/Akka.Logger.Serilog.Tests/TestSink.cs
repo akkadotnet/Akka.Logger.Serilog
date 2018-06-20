c4bd981d8ebd79f5772c43035858e994b236c0d3
Aaron Stannard <aaron@petabridge.com>
2018-06-20 16:37:36 -0700

    have basic spec in-place, but need to work on fixing up assertions

---
+++ src/Akka.Logger.Serilog.Tests/TestSink.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Concurrent;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Akka.Logger.Serilog.Tests
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Basic concurrent sink implementation for testing the final output from Serilog
+    /// </summary>
+    public sealed class TestSink : ILogEventSink
+    {
+        public ConcurrentQueue<global::Serilog.Events.LogEvent> Writes { get; private set; } = new ConcurrentQueue<global::Serilog.Events.LogEvent>();
+
+        /// <summary>
+        /// Resets the contents of the queue
+        /// </summary>
+        public void Clear()
+        {
+            Writes = new ConcurrentQueue<LogEvent>();
+        }
+
+        public void Emit(global::Serilog.Events.LogEvent logEvent)
+        {
+            Writes.Enqueue(logEvent);
+        }
+    }
+}
