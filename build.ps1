73042d46808b067a912c92a12d9937ae895759ee
Danthar <deathraven163@gmail.com>
2019-10-01 16:22:49 +0200

    Fix installed nbench version

--- build.ps1
+++ build.ps1
@@ -30,7 +30,7 @@ Param(
 )
 
 $FakeVersion = "4.61.2"
-$NBenchVersion = "1.0.1"
+$NBenchVersion = "1.0.4"
 $DotNetChannel = "LTS";
 $DotNetVersion = "2.1.500";
 $DotNetInstallerUri = "https://raw.githubusercontent.com/dotnet/cli/v$DotNetVersion/scripts/obtain/dotnet-install.ps1";
