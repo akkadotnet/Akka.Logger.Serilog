a8e51d92d6221a86f2eca1b08d1a3888841811c2
Arjen Smits <Deathraven163@gmail.com>
2019-10-05 20:39:56 +0200

    Release 1.3.10 (#67)
    * Upgrade build system to fix CI (#66)
    
      *  Targets net462 for all projects
      *  Adds build-system template which contains yaml files for azure devops builds
      *  Updates build.fsx to fix some bugs and tweak some stuff
    
    * Bump FluentAssertions from 5.3.0 to 5.9.0 (#60)
    
    Bumps [FluentAssertions](https://github.com/fluentassertions/fluentassertions) from 5.3.0 to 5.9.0.
    - [Release notes](https://github.com/fluentassertions/fluentassertions/releases)
    - [Commits](https://github.com/fluentassertions/fluentassertions/compare/5.3.0...5.9.0)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * Bump Akka.TestKit.Xunit2 from 1.3.9 to 1.3.15
    
    Bumps [Akka.TestKit.Xunit2](https://github.com/akkadotnet/akka.net) from 1.3.9 to 1.3.15.
    - [Release notes](https://github.com/akkadotnet/akka.net/releases)
    - [Changelog](https://github.com/akkadotnet/akka.net/blob/1.3.15/RELEASE_NOTES.md)
    - [Commits](https://github.com/akkadotnet/akka.net/compare/v1.3.9...1.3.15)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * Bump Serilog from 2.7.1 to 2.8.0
    
    Bumps [Serilog](https://github.com/serilog/serilog) from 2.7.1 to 2.8.0.
    - [Release notes](https://github.com/serilog/serilog/releases)
    - [Changelog](https://github.com/serilog/serilog/blob/dev/CHANGES.md)
    - [Commits](https://github.com/serilog/serilog/compare/v2.7.1...v2.8.0)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * Bump Akka from 1.3.9 to 1.3.15
    
    Bumps [Akka](https://github.com/akkadotnet/akka.net) from 1.3.9 to 1.3.15.
    - [Release notes](https://github.com/akkadotnet/akka.net/releases)
    - [Changelog](https://github.com/akkadotnet/akka.net/blob/1.3.15/RELEASE_NOTES.md)
    - [Commits](https://github.com/akkadotnet/akka.net/compare/v1.3.9...1.3.15)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * Update RELEASE_NOTES.md
    
    * Update common.props
    
    * Update RELEASE_NOTES.md

--- build.fsx
+++ build.fsx
@@ -10,13 +10,20 @@ open Fake.DotNetCli
 open Fake.DocFxHelper
 
 // Information about the project for Nuget and Assembly info files
+let product = "Akka.Logger.Serilog"
 let configuration = "Release"
+
+// Metadata used when signing packages and DLLs
+let signingName = "Akka.Logger.Serilog"
+let signingDescription = "Akka.Logger plugin for Serilog"
+let signingUrl = ""
+
 
 // Read release notes and version
 let solutionFile = FindFirstMatchingFile "*.sln" __SOURCE_DIRECTORY__  // dynamically look up the solution
 let buildNumber = environVarOrDefault "BUILD_NUMBER" "0"
 let hasTeamCity = (not (buildNumber = "0")) // check if we have the TeamCity environment variable for build # set
-let preReleaseVersionSuffix = (if (not (buildNumber = "0")) then (buildNumber) else "") + "-beta"
+let preReleaseVersionSuffix = "beta" + (if (not (buildNumber = "0")) then (buildNumber) else DateTime.UtcNow.Ticks.ToString())
 let versionSuffix = 
     match (getBuildParam "nugetprerelease") with
     | "dev" -> preReleaseVersionSuffix
@@ -34,6 +41,8 @@ let outputPerfTests = __SOURCE_DIRECTORY__ @@ "PerfResults"
 let outputNuGet = output @@ "nuget"
 
 Target "Clean" (fun _ ->
+    ActivateFinalTarget "KillCreatedProcesses"
+
     CleanDir output
     CleanDir outputTests
     CleanDir outputPerfTests
@@ -46,14 +55,6 @@ Target "AssemblyInfo" (fun _ ->
     XmlPokeInnerText "./src/common.props" "//Project/PropertyGroup/PackageReleaseNotes" (releaseNotes.Notes |> String.concat "\n")
 )
 
-Target "RestorePackages" (fun _ ->
-    DotNetCli.Restore
-        (fun p -> 
-            { p with
-                Project = solutionFile
-                NoCache = false })
-)
-
 Target "Build" (fun _ ->          
     DotNetCli.Build
         (fun p -> 
@@ -61,6 +62,7 @@ Target "Build" (fun _ ->
                 Project = solutionFile
                 Configuration = configuration }) // "Rebuild"  
 )
+
 
 //--------------------------------------------------------------------------------
 // Tests targets 
@@ -92,15 +94,15 @@ Target "RunTests" (fun _ ->
     let runSingleProject project =
         let arguments =
             match (hasTeamCity) with
-            | true -> (sprintf "xunit -c Release -nobuild -parallel none -teamcity -xml %s_xunit.xml" (outputTests @@ fileNameWithoutExt project))
-            | false -> (sprintf "xunit -c Release -nobuild -parallel none -xml %s_xunit.xml" (outputTests @@ fileNameWithoutExt project))
+            | true -> (sprintf "test -c Release --no-build --logger:trx --logger:\"console;verbosity=normal\" --results-directory %s -- -parallel none -teamcity" (outputTests))
+            | false -> (sprintf "test -c Release --no-build --logger:trx --logger:\"console;verbosity=normal\" --results-directory %s -- -parallel none" (outputTests))
 
         let result = ExecProcess(fun info ->
             info.FileName <- "dotnet"
             info.WorkingDirectory <- (Directory.GetParent project).FullName
             info.Arguments <- arguments) (TimeSpan.FromMinutes 30.0) 
         
-        ResultHandling.failBuildIfXUnitReportedError TestRunnerErrorLevel.DontFailBuild result
+        ResultHandling.failBuildIfXUnitReportedError TestRunnerErrorLevel.Error result  
 
     projects |> Seq.iter (log)
     projects |> Seq.iter (runSingleProject)
@@ -110,7 +112,7 @@ Target "NBench" <| fun _ ->
     let nbenchTestPath = findToolInSubPath "NBench.Runner.exe" (toolsDir @@ "NBench.Runner*")
     printfn "Using NBench.Runner: %s" nbenchTestPath
 
-    let nbenchTestAssemblies = !! "./src/**/*Tests.Performance.dll" // doesn't support .NET Core at the moment
+    let nbenchTestAssemblies = !! "./src/**/bin/**/*Tests.Performance.dll" // doesn't support .NET Core at the moment
 
     let runNBench assembly =
         let includes = getBuildParam "include"
@@ -136,10 +138,61 @@ Target "NBench" <| fun _ ->
             info.FileName <- nbenchTestPath
             info.WorkingDirectory <- (Path.GetDirectoryName (FullName nbenchTestPath))
             info.Arguments <- args) (System.TimeSpan.FromMinutes 45.0) (* Reasonably long-running task. *)
+        
+        printfn "result was %i" result
+
         if result <> 0 then failwithf "NBench.Runner failed. %s %s" nbenchTestPath args
     
     nbenchTestAssemblies |> Seq.iter runNBench
 
+
+//--------------------------------------------------------------------------------
+// Code signing targets
+//--------------------------------------------------------------------------------
+Target "SignPackages" (fun _ ->
+    let canSign = hasBuildParam "SignClientSecret" && hasBuildParam "SignClientUser"
+    if(canSign) then
+        log "Signing information is available."
+        
+        let assemblies = !! (outputNuGet @@ "*.nupkg")
+
+        let signPath =
+            let globalTool = tryFindFileOnPath "SignClient.exe"
+            match globalTool with
+                | Some t -> t
+                | None -> if isWindows then findToolInSubPath "SignClient.exe" "tools/signclient"
+                          elif isMacOS then findToolInSubPath "SignClient" "tools/signclient"
+                          else findToolInSubPath "SignClient" "tools/signclient"
+
+        let signAssembly assembly =
+            let args = StringBuilder()
+                    |> append "sign"
+                    |> append "--config"
+                    |> append (__SOURCE_DIRECTORY__ @@ "appsettings.json") 
+                    |> append "-i"
+                    |> append assembly
+                    |> append "-r"
+                    |> append (getBuildParam "SignClientUser")
+                    |> append "-s"
+                    |> append (getBuildParam "SignClientSecret")
+                    |> append "-n"
+                    |> append signingName
+                    |> append "-d"
+                    |> append signingDescription
+                    |> append "-u"
+                    |> append signingUrl
+                    |> toText
+
+            let result = ExecProcess(fun info -> 
+                info.FileName <- signPath
+                info.WorkingDirectory <- __SOURCE_DIRECTORY__
+                info.Arguments <- args) (System.TimeSpan.FromMinutes 5.0) (* Reasonably long-running task. *)
+            if result <> 0 then failwithf "SignClient failed.%s" args
+
+        assemblies |> Seq.iter (signAssembly)
+    else
+        log "SignClientSecret not available. Skipping signing"
+)
 
 //--------------------------------------------------------------------------------
 // Nuget targets 
@@ -159,7 +212,7 @@ Target "CreateNuget" (fun _ ->
                 { p with
                     Project = project
                     Configuration = configuration
-                    AdditionalArgs = ["--include-symbols"]
+                    AdditionalArgs = ["--include-symbols --no-build"]
                     VersionSuffix = overrideVersionSuffix project
                     OutputPath = outputNuGet })
 
@@ -210,6 +263,19 @@ Target "DocFx" (fun _ ->
 )
 
 //--------------------------------------------------------------------------------
+// Cleanup
+//--------------------------------------------------------------------------------
+
+FinalTarget "KillCreatedProcesses" (fun _ ->
+    log "Shutting down dotnet build-server"
+    let result = ExecProcess(fun info -> 
+            info.FileName <- "dotnet"
+            info.WorkingDirectory <- __SOURCE_DIRECTORY__
+            info.Arguments <- "build-server shutdown") (System.TimeSpan.FromMinutes 2.0)
+    if result <> 0 then failwithf "dotnet build-server shutdown failed"
+)
+
+//--------------------------------------------------------------------------------
 // Help 
 //--------------------------------------------------------------------------------
 
@@ -219,11 +285,12 @@ Target "Help" <| fun _ ->
       "./build.ps1 [target]"
       ""
       " Targets for building:"
-      " * Build      Builds"
-      " * Nuget      Create and optionally publish nugets packages"
-      " * RunTests   Runs tests"
-      " * All        Builds, run tests, creates and optionally publish nuget packages"
-      " * DocFx      Creates a DocFx-based website for this solution"
+      " * Build         Builds"
+      " * Nuget         Create and optionally publish nugets packages"
+      " * SignPackages  Signs all NuGet packages, provided that the following arguments are passed into the script: SignClientSecret={secret} and SignClientUser={username}"
+      " * RunTests      Runs tests"
+      " * All           Builds, run tests, creates and optionally publish nuget packages"
+      " * DocFx         Creates a DocFx-based website for this solution"
       ""
       " Other Targets"
       " * Help       Display this help" 
@@ -238,21 +305,22 @@ Target "All" DoNothing
 Target "Nuget" DoNothing
 
 // build dependencies
-"Clean" ==> "RestorePackages" ==> "AssemblyInfo" ==> "Build" ==> "BuildRelease"
+"Clean" ==> "AssemblyInfo" ==> "Build" ==> "BuildRelease"
 
 // tests dependencies
+"Build" ==> "RunTests"
 
 // nuget dependencies
-"Clean" ==> "RestorePackages" ==> "Build" ==> "CreateNuget"
-"CreateNuget" ==> "PublishNuget" ==> "Nuget"
+"Clean" ==> "Build" ==> "CreateNuget"
+"CreateNuget" ==> "SignPackages" ==> "PublishNuget" ==> "Nuget"
 
 // docs
-"BuildRelease" ==> "Docfx"
+"Clean" ==> "BuildRelease" ==> "Docfx"
 
 // all
 "BuildRelease" ==> "All"
 "RunTests" ==> "All"
-//"NBench" ==> "All"
+"NBench" ==> "All"
 "Nuget" ==> "All"
 
 RunTargetOrDefault "Help"
