5db570e31f847ceea9bc1fbba5d147b8de77044a
Aaron Stannard <aaron@petabridge.com>
2020-03-02 12:38:30 -0600

    upgraded build system to latest

---
+++ src/Akka.Logger.Serilog.Tests.Performance/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using NBench;
+
+namespace Petabridge.Library.Tests.Performance
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return NBenchRunner.Run<Program>();
+        }
+    }
+}
