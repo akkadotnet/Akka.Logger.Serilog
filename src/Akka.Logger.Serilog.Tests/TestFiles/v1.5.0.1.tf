7b5a77a505aea726352d4720ec29e2036fc73b25
Gregorius Soedharmo <arkatufus@yahoo.com>
2023-08-31 06:38:05 +0700

    Add v1.5.0.1 log output data

---
+++ src/Akka.Logger.Serilog.Tests/TestFiles/v1.5.0.1.tf
@@ -0,0 +1,49 @@
+Boolean: True
+Byte: 42
+SByte: "-42"
+Char: A
+Decimal: 3.1415
+Double: 3.1415
+Float: 3.1415
+Int: -42
+UInt: 42
+Long: -42
+ULong: 42
+Short: -42
+UShort: 42
+String: "Fourty Two"
+Enum: One
+BooleanArray: [False, True, False]
+ByteArray: "010203"
+SByteArray: "FF02FD"
+CharArray: [1, 2, 3]
+DecimalArray: [1, -2, 3]
+DoubleArray: [1, -2, 3]
+FloatArray: [1, -2, 3]
+IntArray: [1, -2, 3]
+UIntArray: [1, 2, 3]
+LongArray: [1, -2, 3]
+ULongArray: [1, 2, 3]
+ShortArray: [1, -2, 3]
+UShortArray: [1, 2, 3]
+StringArray: ["One", "Two", "Three"]
+EnumArray: [One, Two, Three]
+BooleanList: [False, True, False]
+ByteList: [1, 2, 3]
+SByteList: ["-1", "2", "-3"]
+CharList: [1, 2, 3]
+DecimalList: [1, -2, 3]
+DoubleList: [1, -2, 3]
+FloatList: [1, -2, 3]
+IntList: [1, -2, 3]
+UIntList: [1, 2, 3]
+LongList: [1, -2, 3]
+ULongList: [1, 2, 3]
+ShortList: [1, -2, 3]
+UShortList: [1, 2, 3]
+StringList: ["One", "Two", "Three"]
+EnumList: [One, Two, Three]
+PrimitiveDictionary: [("Boolean": True), ("Byte": 42), ("SByte": "-42"), ("Char": A), ("Decimal": 3.1415), ("Double": 3.1415), ("Float": 3.1415), ("Int": -42), ("UInt": 42), ("Long": -42), ("ULong": 42), ("Short": -42), ("UShort": 42), ("String": "Fourty Two"), ("Enum": One)]
+ArrayDictionary: [("BooleanArray": [False, True, False]), ("ByteArray": "010203"), ("SByteArray": "FF02FD"), ("CharArray": [1, 2, 3]), ("DecimalArray": [1, -2, 3]), ("DoubleArray": [1, -2, 3]), ("FloatArray": [1, -2, 3]), ("IntArray": [1, -2, 3]), ("UIntArray": [1, 2, 3]), ("LongArray": [1, -2, 3]), ("ULongArray": [1, 2, 3]), ("ShortArray": [1, -2, 3]), ("UShortArray": [1, 2, 3]), ("StringArray": ["One", "Two", "Three"]), ("EnumArray": [One, Two, Three])]
+ListDictionary: [("BooleanList": [False, True, False]), ("ByteList": [1, 2, 3]), ("SByteList": ["-1", "2", "-3"]), ("CharList": [1, 2, 3]), ("DecimalList": [1, -2, 3]), ("DoubleList": [1, -2, 3]), ("FloatList": [1, -2, 3]), ("IntList": [1, -2, 3]), ("UIntList": [1, 2, 3]), ("LongList": [1, -2, 3]), ("ULongList": [1, 2, 3]), ("ShortList": [1, -2, 3]), ("UShortList": [1, 2, 3]), ("StringList": ["One", "Two", "Three"]), ("EnumList": [One, Two, Three])]
+PlainClassInstance: "Akka.Logger.Serilog.Tests.Generator.PlainClassInstance"
